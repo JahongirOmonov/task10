1b4545bdb8e7d7f7965e2d31aeafadde420ce027
Jahongir <dodajoy0777@gmail.com>
2023-09-02 04:03:45 +0500

    ll

---
+++ polls/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import university, sumka
+
+class universitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=university
+        fields=('name', 'filiallari', 'kantrakti')
+
+class sumkaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=sumka
+        fields=('color', 'invented_country')
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import university, sumka
 from django.http import JsonResponse
+from .serializer import sumkaSerializer, universitySerializer
 
 # Create your views here.
 
 def all(request):
-    pustoy=[]
     each = university.objects.all()
-    for i in each:
-        pustoy.append({
-            'Universitet nomi':i.name,
-            'Universitet filillari':i.filiallari,
-            'Universitet kantrakti':i.kantrakti
-        })
-    return JsonResponse(pustoy, safe=False)
+    pustoy=universitySerializer(each, many=True)
+    # for i in each:
+    #     pustoy.append({
+    #         'Universitet nomi':i.name,
+    #         'Universitet filillari':i.filiallari,
+    #         'Universitet kantrakti':i.kantrakti
+    #     })
+    return JsonResponse(pustoy.data, safe=False)
 
 def detail(request, forid):
-    some = get_object_or_404(sumka, id=forid)
-    fornow = {'ranggi':some.color,'ishlab chiqarilgan mamlakat':some.invented_country}
-    return JsonResponse(fornow, safe=False)
+    # some = get_object_or_404(sumka, id=forid)
+    # fornow = {'ranggi':some.color,'ishlab chiqarilgan mamlakat':some.invented_country}
+    some=sumka.objects.filter(id=forid)
+    forsure=sumkaSerializer(some, many=True)
+    return JsonResponse(forsure.data, safe=False)
