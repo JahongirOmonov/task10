a12490cca81c24372498baf58b93ceb280e3c6ac
Jahongir <dodajoy0777@gmail.com>
2023-09-07 07:31:51 +0500

    k

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2ogz+9@8iml_l6)8pz40m*7vy&5@c92@t0$he#on7^qbivbz2('
+SECRET_KEY = os.getenv('donate')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
